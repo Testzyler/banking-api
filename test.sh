73bf99036322926926cfb5d6e98de220c5ed0fad
Atittarn R <atittarn.ruangrit@hotmail.com>
2025-08-05 12:08:27 +0700

    chore: update test script to remove commented-out commands for clarity

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# cd src && go test -race ./... 
+# cd src && go test -coverprofile=coverage.out ./... && go tool cover -func=coverage.out
+
 cd src && go test -coverprofile=coverage.out ./app/features/...
 
 go tool cover -html=coverage.out -o coverage.html
